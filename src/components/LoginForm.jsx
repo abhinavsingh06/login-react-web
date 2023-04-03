61e380b7213542bba666be525e01713885fc8679
abhinav <singh.abhinav6996@gmail.com>
2023-04-03 22:39:55 +0530

    chore: add form boilerplate

--- src/components/LoginForm.jsx
+++ src/components/LoginForm.jsx
@@ -4,14 +4,27 @@ import "react-toastify/dist/ReactToastify.css";
 import "../stylesheets/loginform.css";
 
 const LoginForm = () => {
+  const [email, setEmail] = useState("");
+  const [otp, setOtp] = useState("");
+  const [isOtpSent, setIsOtpSent] = useState(false);
+  const [counter, setCounter] = useState(30);
+  const [isResendDisabled, setIsResendDisabled] = useState(false);
+  const [prevEmail, setPrevEmail] = useState("");
+  const [prevCounter, setPrevCounter] = useState(30);
 
-  const handleEmailSubmit = () => {
+  const handleEmailSubmit = () => {};
 
-  }
+  const handleChange = (event) => {
+    setEmail(event.target.value);
+  };
 
-  const handleChange = () => {
+  const handleOtpChange = () => {};
 
-  }
+  const handleOtpSubmit = () => {};
+
+  const handleBack = () => {};
+
+  const handleSendOtp = () => {};
 
   return (
     <>
@@ -20,13 +33,42 @@ const LoginForm = () => {
         <label for="username">Username</label>
         <input
           type="email"
-          // value={email}
+          value={email}
           onChange={handleChange}
           required
           placeholder="Email"
           id="username"
         />
         <button type="submit">Send OTP</button>
+      </form>
+
+      <form onSubmit={handleOtpSubmit} className="">
+        <h3>Login Here</h3>
+        <label for="otp">OTP:</label>
+        <input
+          type="password"
+          value={otp}
+          onChange={handleOtpChange}
+          placeholder="Enter OTP"
+          id="otp"
+        />
+        <div>
+          <div className="btn-wrapper">
+            <button type="submit" className="btn">
+              Submit
+            </button>
+            <button type="button" onClick={handleBack} className="btn">
+              Back
+            </button>
+          </div>
+          <button
+            type="button"
+            onClick={handleSendOtp}
+            disabled={isResendDisabled}
+          >
+            {isResendDisabled ? `Resend OTP in ${counter}s` : "Resend OTP"}
+          </button>
+        </div>
       </form>
     </>
   );
